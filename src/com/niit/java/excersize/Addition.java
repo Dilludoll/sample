ae7ce57918be71521c26ef0d8606e1095ba597ad
dilludoll <dilshad02247200@gmail.com>
2018-05-13 16:15:06 -0700

    frst commit

---
+++ src/com/niit/java/excersize/Addition.java
@@ -0,0 +1,13 @@
+package com.niit.java.excersize;
+import java.util.*;
+
+public class Addition {
+	public int add(int x,int y) {
+		return x+y;
+	}
+
+	
+
+	
+
+}
---
+++ src/com/niit/java/test/AdditionTest.java
@@ -0,0 +1,12 @@
+package com.niit.java.test;
+
+import com.niit.java.excersize.Addition;
+
+public class AdditionTest {
+	
+	public static void main(String[] args) {
+		Addition add = new Addition();
+		System.out.println(add.add(23,32));
+	}
+
+}
